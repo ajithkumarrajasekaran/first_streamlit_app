17471b7b8d6c1875ec04ad2adb3a0671c5ed3283
ajithkumarrajasekaran <144331778+ajithkumarrajasekaran@users.noreply.github.com>
2023-09-07 15:16:06 +0530

    Update streamlit_app.py
    B'fst menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Testing the code')
+streamlit.title('My Parents healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinch & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
