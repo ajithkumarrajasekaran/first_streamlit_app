f99d7c90ea6d2717b46c69c21cf4c9c1536b1899
ajithkumarrajasekaran <144331778+ajithkumarrajasekaran@users.noreply.github.com>
2023-09-07 15:18:54 +0530

    Update streamlit_app.py
    Added emoji and fruits sections

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('My Parents healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinch & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text(' 🥗 Kale, Spinch & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
